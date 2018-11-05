1cc327ef59debab19bd26c6a8bd0237076581491
Aleksandr Shepelev <a.shepelev@innosoft.pro>
2018-11-05 20:08:20 +0500

    добавила веб-сервер

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+
+@app.route('/hello')
+def hello_world2():
+    return render_template('hello2.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
